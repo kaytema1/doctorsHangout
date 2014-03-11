6dedab65b85057eba1aea336f21ef02597bbac7f
Emmanuel Addo-Yirenkyi <emmanuel.addo.yirenkyi@gmail.com>
2014-03-11 14:49:12 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DoctorsHangout::Application.config.secret_token = '3e6f35ce070ebdb7b94b8823d6dc6d4b6e8126524275214c5d09bae1ba1f8567a50f5fb2fb13edfd155ef80fddd884b004d0971da83a981d76c711114cc19ca2'
